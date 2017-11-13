cbf3cb545b8a5091fcb569f1a4bf0de9ff1f0cbb
Teephop Keerasuntonpong <teephopk@u.northwestern.edu>
2017-11-12 22:47:27 -0600

    centered isotope and tweaked flood page

--- flood.js
+++ flood.js
@@ -3,12 +3,15 @@ function openTime(evt, timePeriod) {
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
+        tabcontent[i].className.replace(" active", "");
     }
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
+        
     }
     document.getElementById(timePeriod).style.display = "block";
+    document.getElementById(timePeriod).className+= " active";
     evt.currentTarget.className += " active";
 }
 
